996ea572de7d35d3b5cc824d3901d6f5391f4877
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2026-05-17 10:14:57 +0100

    zvsdskfafjafajfl

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     base: "/",
+
     server: {
       allowedHosts: true,
     },
-    // Yeh niche wali line process error ko jadd se khatam kar degi
+
     define: {
-      "process.env": {},
-      "process.browser": true,
+      global: "globalThis",
+      process: {
+        env: {},
+      },
     },
   },
 });
