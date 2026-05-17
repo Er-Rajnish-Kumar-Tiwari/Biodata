07285813871cd55d2f4487ef4ae1263463968241
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2026-05-17 10:09:30 +0100

    process done with resonve error

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
+    base: "/",
     server: {
       allowedHosts: true,
+    },
+    // Yeh niche wali line process error ko jadd se khatam kar degi
+    define: {
+      "process.env": {},
+      "process.browser": true,
     },
   },
 });
